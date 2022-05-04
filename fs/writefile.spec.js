becb6350d9e51106aed94bd5b18bf1ea3d09da8b
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2022-05-03 21:13:28 -0500

    demo 3 start

--- fs/writefile.spec.js
+++ fs/writefile.spec.js
@@ -1,13 +1,26 @@
 import { readFile } from 'fs/promises';
 import { writeFileSlowly } from './writeFileSlowly.js';
-import tmp from 'tmp';
 
 test('thing 1', async () => {
   const val = 'THING 1';
-  const fileName = tmp.fileSync().name;
+  const fileName = '.test-output/test.txt';
 
+  // console.log(Date.now() % 100000, 'write thing 1');
   await writeFileSlowly(fileName, val);
 
+  // console.log(Date.now() % 100000, 'read thing 1');
+  const actual = await readFile(fileName, { encoding: 'utf-8' });
+  expect(actual).toBe(val);
+});
+
+test('thing 2', async () => {
+  const val = 'THING 2';
+  const fileName = '.test-output/test.txt';
+
+  // console.log(Date.now() % 100000, 'write thing 2');
+  await writeFileSlowly(fileName, val);
+
+  // console.log(Date.now() % 100000, 'read thing 2');
   const actual = await readFile(fileName, { encoding: 'utf-8' });
   expect(actual).toBe(val);
 });
